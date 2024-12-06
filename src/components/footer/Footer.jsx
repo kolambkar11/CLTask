b10dc20c7f2a538eb27bcc0fedad6254894e462e
kolambkar11 <kolambkarashutosh@gmail.com>
2024-12-06 17:31:07 +0530

    push code to github

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,42 @@
 import React from "react";
 import "./footer.css";
+import logo from "../../assets/img/footer-logo.svg";
 const Footer = () => {
   return (
     <>
-      <section className="footer">
-        <div>Footer</div>
+      <section className="footer-main">
+        <div className="footer">
+          <div className="left">
+            <img src={logo} alt="" />
+            <h3>InvestSphere Company Limited, </h3>
+            <p>
+              House No. 25, Green Meadows ColonySector 10, Rajendra
+              Nagar Jaipur, Rajasthan,Pin Code: 302017
+            </p>
+            <p>Email: customersupport@icicilombard.com Fax no - 022 61961323</p>
+            <div className="social-media">
+              <i class="fa fa-facebook-f"></i>
+              <i class="fa fa-linkedin"></i>
+              <i class="fa fa-facebook-f"></i>
+              <i class="fa fa-youtube"></i>
+              <i class="fa fa-instagram"></i>
+            </div>
+          </div>
+          <div className="right">
+            <p className="footer-paragraph">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum."
+            </p>
+            <h3 className="quicklinks">QuickLinks</h3>
+            Home Car Insurance | TwoWheeler Insurance | Health Insurance |
+            Travel Insurance | Other Insurance | Claims
+          </div>
+        </div>
       </section>
     </>
   );
